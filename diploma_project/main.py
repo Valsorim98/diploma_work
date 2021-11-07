a71d61177f7368f7c0aa2b316c60109ff3cb8083
Valsorim98 <vt.stanchev@gmail.com>
2021-11-07 17:43:20 +0200

    Create main.py with main function

---
+++ diploma_project/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from gui import GUI
+
+def main():
+    """Main function for the project.
+    """
+
+    gui = GUI()
+
+if __name__ == "__main__":
+    main()
