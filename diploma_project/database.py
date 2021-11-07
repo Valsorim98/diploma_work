1add3e4b44760a1d1c93bca0a840f23e9a2a1437
Valsorim98 <vt.stanchev@gmail.com>
2021-11-07 17:52:17 +0200

    Create database.py with methods to connect to the database and reach the documents in it

---
+++ diploma_project/database.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+
+from pymongo import MongoClient
+import certifi
+
+# from gui import GUI
+
+class Database():
+    """Class database.
+    """
+
+# Region attributes.
+
+    __client = None
+    """Connect as a client.
+    """
+
+# End region.
+
+# Region constructor.
+
+    def __init__(self):
+        """Constructor for database class.
+        """
+
+        self.__get_documents()
+
+# End region.
+
+# Region private methods.
+
+    def __connection_database(self):
+        """Method to connect to the database.
+        """
+
+        # Use SSL for the connection to the cluster.
+        ca = certifi.where()
+
+        # MongoDB URL to connect to the cluster.
+        url = "mongodb+srv://analytics:analytics-password@mflix.jkfp1.mongodb.net/Hotels?retryWrites=true&w=majority"
+
+        # Connect to the database with the cluster URL and SSL.
+        self.__client = MongoClient(url, tlsCAFile=ca)
+
+        return self.__client["Hotels"]
+
+    def __get_documents(self):
+        """Method to get the existing documents in the collection.
+        """
+
+        # Pass the method that connects to the database.
+        db = self.__connection_database()
+
+        # Pass the collection with the hotels.
+        collection = db["Hotel_Info"]
+
+        # Get all the documents from the collection.
+        read_collection = collection.find({})
+        
+        # Iterate through all documents and print them.
+        # for document in read_collection:
+            # print(document)
+
+        # Get the dictionary with the user preferences to search in the database.
+        # GUI.__user_preferences()
+
+# End region.
