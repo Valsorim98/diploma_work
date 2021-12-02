3f997bcef31b4656d8ee123d2b5be3188661efd2
Valsorim98 <vt.stanchev@gmail.com>
2021-12-02 19:28:11 +0200

    Create a form for hotel results on top of main form and fill it with the found hotels

--- diploma_project/gui.py
+++ diploma_project/gui.py
@@ -5,6 +5,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import *
 from tkinter.ttk import Combobox
+import emoji
 
 from database import Database
 
@@ -106,15 +107,15 @@ class GUI():
         """Constructor for GUI class.
         """
 
-        self.__create_form()
+        self.__main_form()
 
-        self.__create_labels()
+        self.__main_labels()
 
-        self.__create_comboboxes()
+        self.__main_comboboxes()
 
-        self.__create_checkboxes()
+        self.__main_checkboxes()
 
-        self.__create_button()
+        self.__main_button()
 
         self.__positioning()
 
@@ -134,8 +135,8 @@ class GUI():
 
 # Region private methods.
 
-    def __create_form(self):
-        """Method to create the GUI form.
+    def __main_form(self):
+        """Method to create the main GUI form.
         """
 
         # Create the window for GUI.
@@ -148,18 +149,142 @@ class GUI():
         self.__root.resizable(False, False)
 
         # Set window size.
-        root_width = 600
-        root_height = 600
+        _root_width = 600
+        _root_height = 600
 
         # Place the window in the center of the screen.
-        screen_width = self.__root.winfo_screenwidth()
-        screen_height = self.__root.winfo_screenheight()
-        x_cordinate = int((screen_width/2) - (root_width/2))
-        y_cordinate = int((screen_height/2) - (root_height/2))
-        self.__root.geometry("{}x{}+{}+{}".format(root_width, root_height, x_cordinate, y_cordinate))
+        _screen_width = self.__root.winfo_screenwidth()
+        _screen_height = self.__root.winfo_screenheight()
+        _x_cordinate = int((_screen_width/2) - (_root_width/2))
+        _y_cordinate = int((_screen_height/2) - (_root_height/2))
+        self.__root.geometry("{}x{}+{}+{}".format(_root_width, _root_height, _x_cordinate, _y_cordinate))
 
         # Set window background colour.
         self.__root.configure(bg="#1C86EE")
+
+    def __results_form(self, found_hotels):
+        """Method to show create a new form with the hotel results.
+        """
+
+        _found_hotels = found_hotels
+
+        _root_results = Toplevel(self.__root)
+        _root_results.title("Search results")
+        _root_results.resizable(False, False)
+
+        _root_results_width = 750
+        _root_results_height = 750
+
+        _screen_width = _root_results.winfo_screenwidth()
+        _screen_height = _root_results.winfo_screenheight()
+        _x_cordinate = int((_screen_width/2) - (_root_results_width/2))
+        _y_cordinate = int((_screen_height/2) - (_root_results_height/2))
+        _root_results.geometry("{}x{}+{}+{}".format(_root_results_width, _root_results_height, _x_cordinate, _y_cordinate))
+
+        _root_results.configure(bg="#1C86EE")
+
+
+
+        # ME and INDIAN:
+
+        # Create frame on the root window.
+        # dont need that
+        # _frame=Frame(_root_results, bg="#1C86EE")
+        # _frame.pack(fill=BOTH, expand="yes")
+
+        # Create canvas on the frame.
+        _canvas = Canvas(_root_results, bg="#1C86EE", bd=0, highlightthickness=0, relief='ridge')
+        _canvas.pack(side=LEFT, expand=True, fill=BOTH)
+
+        # Create the scrollbar on the frame.
+        _scrollbar = Scrollbar(_root_results, orient="vertical", command=_canvas.yview)
+        _scrollbar.pack(side=RIGHT, fill=Y)
+
+        # Create label on the canvas.
+        # _main_label = Label(_canvas, text="Results", fg="white", bg="#1C86EE", font=("arial", 20), pady=20)
+        # _main_label.pack()
+
+        # Make the canvas scrollable.
+        _canvas.config(yscrollcommand=_scrollbar.set)
+        _canvas.bind('<Configure>', lambda e: _canvas.configure(scrollregion=_canvas.bbox("all")))
+
+        # Create a frame on the canvas.
+        frame_in_canvas = Frame(_canvas, bg="#1C86EE", pady=30)
+
+        # Populate the frame on the canvas with the text boxes.
+        # Coords start with (x, y).
+        _canvas.create_window((240,0), window=frame_in_canvas, anchor="nw")
+
+        # Create a text box for every found hotel.
+        for hotel in _found_hotels.values():
+
+            # For star emoji:
+            star = emoji.emojize(":star:")
+
+            # Starting message.
+            msg = f'''\nCity: {hotel["Town"]}\nName: {hotel["Hotel"]}\nStars: '''
+            stars_msg = ""
+
+            # Add number of stars equal to the star rating of the hotel.
+            for i in range (hotel["Star Rating"]):
+                stars_msg = stars_msg + star
+
+            price_msg = hotel["Price"]
+
+            # Concatenate strings.
+            message = msg + stars_msg + f"\nPrice: {price_msg} BGN"
+
+            # Concatenate string with wi-fi.
+            if hotel["Wi-fi"] == True:
+                wi_fi_msg = "\nWi-fi: Yes"
+                message = message + wi_fi_msg
+            else:
+                wi_fi_msg = "\nWi-fi: No"
+                message = message + wi_fi_msg
+
+            # Concatenate string with air conditioner.
+            if hotel["Air Conditioner"] == True:
+                ac_msg = "\nAir conditioner: Yes"
+                message = message + ac_msg
+            else:
+                ac_msg = "\nAir conditioner: No"
+                message = message + ac_msg
+
+            # Concatenate string with bar.
+            if hotel["Bar"] == True:
+                bar_msg = "\nBar: Yes"
+                message = message + bar_msg
+            else:
+                bar_msg = "\nBar: No"
+                message = message + bar_msg
+
+            # Concatenate string with restaurant.
+            if hotel["Restaurant"] == True:
+                restaurant_msg = "\nRestaurant: Yes"
+                message = message + restaurant_msg
+            else:
+                restaurant_msg = "\nRestaurant: No"
+                message = message + restaurant_msg
+
+            # Concatenate string with pets.
+            if hotel["Allow Pets"] == True:
+                pets_msg = "\nAllow pets: Yes"
+                message = message + pets_msg
+            else:
+                pets_msg = "\nAllow pets: No"
+                message = message + pets_msg
+
+            text_box = Text(frame_in_canvas, height=11, width=32, font=("Courier", 10, "italic"))
+            text_box.pack(pady=10)
+            text_box.insert('end', message)
+            # Make the text box not editable.
+            text_box.config(state='disabled')
+
+            text_box.tag_add("star", "4.7", "4.15")
+            text_box.tag_config("star", background="white", foreground="red")
+
+        # Focus the newly created form.
+        _root_results.after(1, lambda: _root_results.focus_force())
 
     def __searchdb_command(self):
         """Method to get the user input to search in the database.
@@ -169,11 +294,13 @@ class GUI():
 
         # Call Database class.
         self.__database = Database(__user_preferences)
-        found_hotels = self.__database.suitable_hotels
-        print(f"\nIn GUI {found_hotels}")
+        _found_hotels = self.__database.suitable_hotels
 
-    def __create_labels(self):
-        """Method to create the labels.
+        # Create a new form with the results.
+        self.__results_form(found_hotels=_found_hotels)
+
+    def __main_labels(self):
+        """Method to create the labels in main form.
         """
 
         self.__main_label = tk.Label(text="Looking for a hotel?\nYou are at the right place!", fg="white", bg="#1C86EE")
@@ -191,8 +318,8 @@ class GUI():
         self.__price_label = tk.Label(self.__root, text="Price:", fg="white", bg="#1C86EE")
         self.__price_label.config(font=("Courier", 10))
 
-    def __create_comboboxes(self):
-        """Method to create the comboboxes.
+    def __main_comboboxes(self):
+        """Method to create the comboboxes in main form.
         """
 
         self.__city_combobox = Combobox(self.__root, width=25)
@@ -205,27 +332,27 @@ class GUI():
         self.__price_combobox = Combobox(self.__root, width=25)
         self.__price_combobox['values'] = ["Up to 30 BGN", "31-50 BGN", "51-70 BGN", "71-100 BGN", "More than 100 BGN"]
 
-    def __create_checkboxes(self):
-        """Method to create the checkboxes.
+    def __main_checkboxes(self):
+        """Method to create the checkboxes in main form.
         """
 
         self.__wi_fi_var = BooleanVar()
-        self.__wi_fi_checkbox = Checkbutton(self.__root, text="Wi-fi", bg="#1C86EE", variable=self.__wi_fi_var)
+        self.__wi_fi_checkbox = Checkbutton(self.__root, text="Wi-fi", fg="white", bg="#1C86EE", selectcolor="#1C86EE", variable=self.__wi_fi_var)
 
         self.__ac_var = BooleanVar()
-        self.__ac_checkbox = Checkbutton(self.__root, text="Air Conditioner", bg="#1C86EE", variable=self.__ac_var)
+        self.__ac_checkbox = Checkbutton(self.__root, text="Air Conditioner", fg="white", bg="#1C86EE", selectcolor="#1C86EE", variable=self.__ac_var)
 
         self.__bar_var = BooleanVar()
-        self.__bar_checkbox = Checkbutton(self.__root, text="Bar", bg="#1C86EE", variable=self.__bar_var)
+        self.__bar_checkbox = Checkbutton(self.__root, text="Bar", fg="white", bg="#1C86EE", selectcolor="#1C86EE", variable=self.__bar_var)
 
         self.__restaurant_var = BooleanVar()
-        self.__restaurant_checkbox = Checkbutton(self.__root, text="Restaurant", bg="#1C86EE", variable=self.__restaurant_var)
+        self.__restaurant_checkbox = Checkbutton(self.__root, text="Restaurant", fg="white", bg="#1C86EE", selectcolor="#1C86EE", variable=self.__restaurant_var)
 
         self.__pets_var = BooleanVar()
-        self.__pets_checkbox = Checkbutton(self.__root, text="Allow Pets", bg="#1C86EE", variable=self.__pets_var)
+        self.__pets_checkbox = Checkbutton(self.__root, text="Allow Pets", fg="white", bg="#1C86EE", selectcolor="#1C86EE", variable=self.__pets_var)
 
-    def __create_button(self):
-        """Method to create the button.
+    def __main_button(self):
+        """Method to create the button in main form.
         """
 
         self.__search_btn = tk.Button(
